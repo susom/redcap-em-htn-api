b755e1f705e2e74185c56627cef83036af7d02ef
Irvin Szeto <irvins@stanford.edu>
2021-01-20 20:43:12 -0800

    why add patient broken on prod

--- pages/add_patient.php
+++ pages/add_patient.php
@@ -52,7 +52,115 @@ $showhide   = $page !== "dashboard" ? "hide" : "";
                 <div class="patient_details col-md-12">
                     <div class="bg-light rounded-right rounded-bottom rounded-left p-3 profile panels">
                         <div class="patient_detail mb-5">
-                            
+                            <form method="POST">
+                                <div class="patient_name mb-5 pt-5">
+                                    <fig class="patient_profile d-block mx-auto row">
+                                        <figure class="text-center "><a href="#" class="rounded-circle add_photo d-inline-block">Add Photo</a></figure>
+                                        
+                                        <figcaption class="my-4 col-sm-10 offset-sm-1 row">
+                                            <h3 class="col-sm-12">Patient Contact Details</h3>
+                                            <div class="form-group col-sm-4">
+                                                <label for="patient_fname"><b>Patient First Name</b></label>
+                                                <input type="text" class="form-control" name="patient_fname" id="patient_fname" aria-describedby="patient_fname" placeholder="First Name">
+                                            </div>
+                                            <div class="form-group col-sm-4">
+                                                <label for="patient_mname"><b>Middle Name</b></label>
+                                                <input type="text" class="form-control" name="patient_mname" id="patient_mname" aria-describedby="patient_mname" placeholder="Middle Name">
+                                            </div>
+                                            <div class="form-group col-sm-4">
+                                                <label for="patient_lname"><b>Last Name</b></label>
+                                                <input type="text" class="form-control" name="patient_lname" id="patient_lname" aria-describedby="patient_lname" placeholder="Last Name">
+                                            </div>
+                                    
+                                            <div class="form-group col-sm-12">
+                                                <label for="patient_mrn"><b>Patient MRN</b></label>
+                                                <input type="text" class="form-control" name="patient_mrn" id="patient_mrn" aria-describedby="patient_mrn" placeholder="eg; 123456789">
+                                                <small id="patient_mrn_help" class="form-text text-muted">This will be used to link with STARR/EPIC databses.</small>
+                                            </div>
+                                            
+                                            <div class="form-group col-sm-6">
+                                                <label for="patient_email"><b>Patient Email</b></label>
+                                                <input type="text" class="form-control" name="patient_email" id="patient_email" aria-describedby="patient_email" placeholder="eg; jane@doe.com">
+                                                <small id="patient_email_help" class="form-text text-muted">This will be used to authorize BP data.</small>
+                                            </div>
+
+                                            <div class="form-group col-sm-4">
+                                                <label for="patient_phone"><b>Patient Cell</b></label>
+                                                <input type="text" class="form-control" name="patient_phone" id="patient_phone" aria-describedby="patient_phone" placeholder="eg; 555-555-1234">
+                                                <small id="patient_phone_help" class="form-text text-muted">This will be used to text surveys to patient.</small>
+                                            </div>
+
+                                            <div class="my-4 bg-info text-light py-3 row col-sm-12">
+                                                <h3 class="col-sm-12"><b>Blood Pressure Goals</b></h3>
+                                                <div class="form-group col-sm-4">
+                                                    <label for="patient_bp_target_systolic"><b>Systolic Goal</b></label>
+                                                    <input type="text" class="form-control" name="patient_bp_target_systolic" id="patient_bp_target_systolic" aria-describedby="patient_bp_target_systolic" placeholder="eg; 120">
+                                                </div>
+                                                <div class="form-group col-sm-4">
+                                                    <label for="patient_bp_target_diastolic"><b>Diastolic Goal</b></label>
+                                                    <input type="text" class="form-control" name="patient_bp_target_diastolic" id="patient_bp_target_diastolic" aria-describedby="patient_bp_target_diastolic" placeholder="eg; 80">
+                                                </div>
+                                                <div class="form-group col-sm-4">
+                                                    <label for="patient_bp_target_pulse"><b>Pulse Goal</b></label>
+                                                    <input type="text" class="form-control" name="patient_bp_target_pulse" id="patient_bp_target_pulse" aria-describedby="patient_bp_target_pulse" placeholder="eg; 65">
+                                                </div>
+                                            </div>
+                                        </figcaption>
+                                    </fig>
+                                </div>
+
+                                <div class="patient_details col-sm-10 offset-sm-1 row pt-3">
+                                    <h3 class="col-sm-12">Patient Baseline</h3>
+                                    <em class="col-sm-12 mb-3">Much of this will be pulled and automatically refreshed from STARR/EPIC</em>
+                                    
+                                    <div class="form-group col-sm-6">
+                                        <label><b>Patient Sex*</b></label>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="sex" id="sex1" value="Male">
+                                            <label class="form-check-label" for="sex1">
+                                                Male
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="sex" id="sex2" value="Female">
+                                            <label class="form-check-label" for="sex2">
+                                                Female
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group col-sm-6">
+                                        <label for="patient_mrn"><b>Patient DOB*</b></label>
+                                        <input type="text" class="form-control" name="patient_birthday" id="patient_birthday" aria-describedby="patient_birthday" placeholder="eg; mm/dd/yyyy">
+                                        <small id="patient_birthday_help" class="form-text text-muted">*This will also be pulled from STARR/EPIC databses.</small>
+                                    </div>
+
+                                    <div class="col-sm-12 row">
+                                        <div class="form-group col-sm-4">
+                                            <label for="weight"><b>Weight*</b></label>
+                                            <input type="text" class="form-control" name="weight" id="weight" aria-describedby="weight" placeholder="eg; 170lb">
+                                        </div>
+
+                                        <div class="form-group col-sm-4">
+                                            <label for="height"><b>Height*</b></label>
+                                            <input type="text" class="form-control" name="height" id="height" aria-describedby="height" placeholder="eg; 5'10">
+                                        </div>
+
+                                        <div class="form-group col-sm-4">
+                                            <label for="bmi"><b>BMI*</b></label>
+                                            <input type="text" class="form-control" name="bmi" id="bmi" aria-describedby="bmi" placeholder="eg; 23">
+                                        </div>
+                                    </div>
+
+
+
+                                    
+                                </div>
+
+                                <div class="btns text-center">
+                                    <button type="submit" id="save_patient" class='btn btn-primary btn-lg'>Add Patient</button>
+                                </div>
+                            </form>
                         </div>
                         
                     </div>
